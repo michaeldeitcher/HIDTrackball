25e62e9fdaca4b42606062d3aeddf3f8a79f98ed
Michael Deitcher <michaeldeitcher@gmail.com>
2016-04-18 22:13:57 -0600

    initial commit

---
+++ HIDTrackball/HIDTrackball.cpp
@@ -0,0 +1,9 @@
+//
+//  HIDTrackball.cpp
+//  HIDTrackball
+//
+//  Created by Michael Deitcher on 1/10/16.
+//  Copyright © 2016 StudioSplotch. All rights reserved.
+//
+
+#include "HIDTrackball.hpp"
